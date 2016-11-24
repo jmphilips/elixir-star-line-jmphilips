03b230f6846c89932424f8ff8d743a47ee0a94b7
Joshua Philips <jmichaelphilips@gmail.com>
2016-11-24 00:41:40 -0600

    added some new functions.  Still need to work out kinks

--- lib/star_line.ex
+++ lib/star_line.ex
@@ -1,4 +1,13 @@
 defmodule StarLine do
+
+    # Below is an example of what the final output will look like.
+    #
+    # **********
+    # * Joshua *
+    # **********
+
+
+    # This first group of functions makes the line of stars that will be used at the beginning and end of the line.
     def get_line_length(string) do
         String.length(string)
     end
@@ -14,5 +23,17 @@ defmodule StarLine do
     def make_star_only_line(num) do
         String.duplicate("*", num)
     end
+
+    # This set of functions will take the input and make the line with a string.
     
+    def return_name_line(string) do
+        "* " <> string <> " *"
+    end
+
+
+    def main_combine(string) do
+        return_star_line_length(string)
+        return_name_line(string)
+        return_star_line_length(string)
+    end
 end
