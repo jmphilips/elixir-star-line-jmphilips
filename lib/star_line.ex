4744ca194ca580d5df34b07ad4aba36ec172fe86
Joshua Philips <jmichaelphilips@gmail.com>
2016-11-24 00:17:29 -0600

    Successfully completed the first star line

--- lib/star_line.ex
+++ lib/star_line.ex
@@ -7,5 +7,12 @@ defmodule StarLine do
         num + 4
     end
 
+    def return_star_line_length(string) do
+        string |> get_line_length |> add_two_to_line_length |> make_star_only_line
+    end
+
+    def make_star_only_line(num) do
+        String.duplicate("*", num)
+    end
     
 end
